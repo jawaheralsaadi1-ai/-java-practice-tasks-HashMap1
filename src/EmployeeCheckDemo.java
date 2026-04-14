fb32b68d798059e82dc240d244c370e546f927a0
Jawaher <8alsaadii@gmail.com>
2026-04-14 16:11:35 +0400

    Add Task 3 -  Check Keys and Values in a Java HashMap

---
+++ src/EmployeeCheckDemo.java
@@ -0,0 +1,30 @@
+
+import java.util.HashMap;
+
+
+public class EmployeeCheckDemo {
+
+
+   public static void main(String[] args) {
+        HashMap<Integer, String> empIDsWithNamesMap = new HashMap<>();
+        empIDsWithNamesMap.put(102, "Maha");
+        empIDsWithNamesMap.put(103, "Wala");
+        empIDsWithNamesMap.put(104, "Omar");
+        empIDsWithNamesMap.put(105, "Majid");
+        System.out.println(" Full Employees List : " + empIDsWithNamesMap);
+        boolean ID1 = empIDsWithNamesMap.containsKey("102");
+        System.out.println("check if  ID  with number '102' is exists: Result :  " + ID1);
+        boolean ID2 = empIDsWithNamesMap.containsKey(105);
+       System.out.println("check if second ID  with number '105' is exists: Result :  " + ID2);
+       boolean name1= empIDsWithNamesMap.containsValue("Maha");
+       System.out.println("check if  name  'Maha' is exists: Result :  " + name1);
+       boolean name2= empIDsWithNamesMap.containsValue("Hajer");
+       System.out.println("check if  name  'Omar' is exists: Result :  " + name2);
+
+
+
+
+
+
+   }
+}
